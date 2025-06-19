4ab92e0b6c2d815eb0a894dd49d41f486bb0b255
daveyurbae <davidnebore1217@gmail.com>
2025-06-20 01:00:28 +0700

    modify docker compose & spark train

--- web/app.py
+++ web/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3cd7e55ba6abbb990dd4502f10ce124dea6a59f21278ba6bfb9fe75bc61b4d0
+oid sha256:92ff055b681feff963dead0aff8234af8faf5d0d3d0f8bd7bd0ce852bf7f4ad3
 size 17700
